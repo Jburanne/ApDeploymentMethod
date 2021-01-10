a40ff7d19b88e13158dba45a2fbb6bea0e2f32b4
houwy <houwyvenny@126.com>
2021-01-10 17:12:14 +0800

    我的部署页面-查询

--- ApDeploymentByScp/users/views.py
+++ ApDeploymentByScp/users/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from .models import User
-from .forms import AddForm,LoginForm
+from .forms import AddForm, LoginForm
 from . import readCad
 import os
-from django.shortcuts import HttpResponse,render,redirect
+from django.shortcuts import HttpResponse, render, redirect
+import pymysql
+
+
+def connectToMySQL():
+    conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='hwy960608',
+                           db='apdeployment', charset='utf8')
+    cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
+    return conn, cursor
+
+
+def closeSQLConnection(cursor, conn):
+    cursor.close()
+    conn.close()
 
 
 # Create your views here.
@@ -16,59 +29,80 @@ def uploadCad(request):
             filename = af.cleaned_data['filename']
             headimg = af.cleaned_data['headimg']
             cover_num = af.cleaned_data['cover_num']
-            spread_dist = af.cleaned_data['propagation_distance']*1000
-            reduce_dist = af.cleaned_data['wall_reduction_distance']*1000
-            wall_reduce_dist = af.cleaned_data['wall_reduction_distance']*1000
-            glass_reduce_dist = af.cleaned_data['glass_reduction_distance']*1000
-            wood_reduce_dist = af.cleaned_data['wood_reduction_distance']*1000
-            other_reduce_dist = af.cleaned_data['other_reduction_distance']*1000
+            spread_dist = af.cleaned_data['propagation_distance'] * 1000
+            reduce_dist = af.cleaned_data['wall_reduction_distance'] * 1000
+            wall_reduce_dist = af.cleaned_data['wall_reduction_distance'] * 1000
+            glass_reduce_dist = af.cleaned_data['glass_reduction_distance'] * 1000
+            wood_reduce_dist = af.cleaned_data['wood_reduction_distance'] * 1000
+            other_reduce_dist = af.cleaned_data['other_reduction_distance'] * 1000
 
             time_limit = af.cleaned_data['time_limit']
-            dist_thre = af.cleaned_data['dist_thre']*1000
+            dist_thre = af.cleaned_data['dist_thre'] * 1000
 
-            #写入数据表User中
-            user = User(name=filename,headimg=headimg)
+            # 写入数据表User中
+            user = User(name=filename, headimg=headimg)
             user.save()
             # 读取cad文件
-            cad_file_path = os.getcwd().replace("\\","/")+"/media/img/"+str(headimg)
-            #readCad.saveLines(cad_file_path)
+            cad_file_path = os.getcwd().replace("\\", "/") + "/media/img/" + str(headimg)
+            # readCad.saveLines(cad_file_path)
             readCad.saveLinesByTypes(cad_file_path)
             # c++处理线条&获取算法的输入文件
-            readCad.getInput(spread_dist, wall_reduce_dist,glass_reduce_dist, wood_reduce_dist, other_reduce_dist, cover_num, dist_thre)
+            readCad.getInput(spread_dist, wall_reduce_dist, glass_reduce_dist, wood_reduce_dist, other_reduce_dist,
+                             cover_num, dist_thre)
             # 运行scp代码，输出解
             readCad.runScp(time_limit)
             # 读取解
-            readCad.getResPoints(spread_dist, wall_reduce_dist,glass_reduce_dist, wood_reduce_dist, other_reduce_dist, cover_num, dist_thre)
+            readCad.getResPoints(spread_dist, wall_reduce_dist, glass_reduce_dist, wood_reduce_dist, other_reduce_dist,
+                                 cover_num, dist_thre)
             # 解的可视化
-            img_path,pic_name,ap_num = readCad.readScpRes(spread_dist, reduce_dist, cover_num)
-            #img_path = readCad.readLines()
-            #展示图片
-            #img_data = open(img_path,'rb').read()
-            #return HttpResponse(img_data, content_type="image/png")
-            img_relative_path = '/media/img/'+pic_name+'.png'
+            img_path, pic_name, ap_num = readCad.readScpRes(spread_dist, reduce_dist, cover_num)
+            # img_path = readCad.readLines()
+            # 展示图片
+            # img_data = open(img_path,'rb').read()
+            # return HttpResponse(img_data, content_type="image/png")
+            img_relative_path = '/media/img/' + pic_name + '.png'
             return render(request, 'users/deploymentResult.html',
-                          context={"resimgpath":img_relative_path,"cover_num":cover_num,
-                                   "spread_dist":spread_dist/1000, "time_limit":time_limit,
-                                   "dist_thre":dist_thre/1000,"ap_num":ap_num,
-                                   "filename":filename})
-            #print(os.getcwd())
-            #return render(request, 'users/index.html', context={"user": user})
-            #return HttpResponse("上传成功")
-            #return HttpResponse(cover_num)
+                          context={"resimgpath": img_relative_path, "cover_num": cover_num,
+                                   "spread_dist": spread_dist / 1000, "time_limit": time_limit,
+                                   "dist_thre": dist_thre / 1000, "ap_num": ap_num,
+                                   "filename": filename})
+            # print(os.getcwd())
+            # return render(request, 'users/index.html', context={"user": user})
+            # return HttpResponse("上传成功")
+            # return HttpResponse(cover_num)
     else:
         print(os.getcwd())
         af = AddForm()
-        return render(request, 'users/uploadCad.html', context={'af':af})
+        return render(request, 'users/uploadCad.html', context={'af': af})
+
 
 def deploymentResult(request):
     return render(request, 'users/deploymentResult.html')
 
+
 def add(request):
     af = AddForm()
-    return render(request, 'users/add.html', context={'af':af})
+    return render(request, 'users/add.html', context={'af': af})
+
 
 def myDeployment(request):
-    return render(request, 'users/myDeployment.html')
-
-
+    conn, cursor = connectToMySQL()
+    if request.method == "GET":
+        sqlstr = "select username,c.filename as cadname,c.filepath as cadpath," \
+                 "r.filename as resname, r.filepath as respath,p.* from " \
+                 "(select * from res2cad where userid=1) a " \
+                 "left join (select * from users) u on a.userid=u.id " \
+                 "left join (select * from cad) c on a.cadid=c.id " \
+                 "left join (select * from result) r on a.resid=r.id " \
+                 "left join (select * from params) p on a.paramid=p.id"
+        #print(sqlstr)
+        cursor.execute(sqlstr)
+        res = cursor.fetchall()
+        return render(request, 'users/myDeployment.html', context={"res": res})
+    else:
+        uname = request.POST.get("uname")
+        upwd = request.POST.get("upwd")
+        cursor.execute("insert into users(username,password) values(%s,%s)",[uname,upwd,])
+        conn.commit()
+        return redirect('users/myDeployment.html/')
 
